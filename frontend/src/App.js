a81f53c5d2a740b7deca1600cc65d77445669042
chevano <chevanogordon@gmail.com>
2025-04-08 18:04:28 -0400

    Create List Products and an Array of Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Marxx</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1> List of Products </h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.name}>
+              <a href={`/product/${product.name}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <p> {product.name} </p>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button> Add to cart </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
