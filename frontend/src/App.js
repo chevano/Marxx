0e48c101aebd6dddd0b31539e1ae387f2260c7d3
chevano <chevanogordon@gmail.com>
2025-04-07 21:27:02 -0400

    resolving merge conflict

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Marxx</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
