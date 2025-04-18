1e578735fcda3c9e0144d8889d041bb61bcacaf9
chevano <chevanogordon@gmail.com>
2025-04-18 18:03:44 -0400

    Create Rating and Product Component and adding Bootstrap to HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import { useEffect, useReducer } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -45,22 +47,20 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.name}>
-              <Link to={`/product/${product.name}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.name}`}>
-                  <p> {product.name} </p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button> Add to cart </button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col
+                key={product.name}
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                className="mb-3"
+              >
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
