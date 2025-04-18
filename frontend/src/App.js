739e74b877fc4198593dde49233182372aa95929
chevano <chevanogordon@gmail.com>
2025-04-18 12:37:38 -0400

    Add Bootstrap to Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,33 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Marxx</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Link className="link" to="/">
+                <Navbar.Brand>Marxx</Navbar.Brand>
+              </Link>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:name" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:name" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
