414dffa11414450e8b86097f6b44f092760d90fb
chevano <chevanogordon@gmail.com>
2025-06-05 17:52:12 -0400

    Create the Backend API for the Sign-in Page

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -18,11 +19,20 @@ mongoose
 
 const app = express(); // Initializes a new instance of an Express App
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Instantiate the database with sample data
 app.use('/api/seed', seedRouter);
 
-// Handles all the request Methods that has a starting endpoint of '/api/products'
+// Handles all the request Methods that has a starting endpoint of '/api/products' using productRouter
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+// Handles the errors that occurs in the expressAsyncHandler function which can be found in userRoutes.js
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
