cbe1e0c2c5cfaec64dd07fea374ce1a0ff590f6d
chevano <chevanogordon@gmail.com>
2025-06-16 14:03:24 -0400

    Create Select Payment Method Screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -11,6 +11,9 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
     // Retrive data from the browser if there is any
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -54,6 +57,7 @@ function reducer(state, action) {
         cart: {
           cartItems: [],
           shippingAddress: {},
+          paymentMethod: '',
         },
       };
 
@@ -64,6 +68,12 @@ function reducer(state, action) {
           ...state.cart,
           shippingAddress: action.payload,
         },
+      };
+
+    case 'SAVE_SHIPPING_METHOD':
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
       };
 
     default:
---
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,66 @@
+import React, { useContext, useEffect, useState } from 'react';
+import CheckoutSteps from '../components/CheckoutSteps';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import { Store } from '../Store';
+
+export default function PaymentMethodScreen() {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress, paymentMethod },
+  } = state;
+
+  const [paymentMethodName, setPaymentMethod] = useState(
+    paymentMethod || 'PayPal'
+  );
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate('/shipping');
+    }
+  }, [navigate, shippingAddress]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    ctxDispatch({ type: 'SAVE_PAYMENT_METHOD', payload: paymentMethodName });
+    localStorage.setItem('paymentMethod', paymentMethodName);
+    navigate('/placeorder');
+  };
+
+  return (
+    <div>
+      <CheckoutSteps>steps1 steps2 steps3</CheckoutSteps>
+      <Container className="small-container my-3">
+        <h1 className="my-3">Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="PayPal"
+              label="PayPal"
+              value="PayPal"
+              checked={paymentMethodName === 'PayPal'}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="Stripe"
+              label="Stripe"
+              value="Stripe"
+              checked={paymentMethodName === 'Stripe'}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Button type="submit">Continue</Button>
+          </div>
+        </Form>
+      </Container>
+    </div>
+  );
+}
