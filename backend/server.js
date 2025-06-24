76cee05f6966384f4cc8739bb86afd4931efafe1
chevano <chevanogordon@gmail.com>
2025-06-24 11:59:51 -0400

    testing connection

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,9 @@ const app = express(); // Initializes a new instance of an Express App
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (res, req) => {
+  res.send('testing home route');
+});
 app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
